e7c29d834d958db9eee5494df372071a378dd6f2
Rupali <rupis2006@gmail.com>
2026-06-07 18:42:17 +0530

    Create program8.c

---
+++ day-2/program8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int palindrome(int n) {
+    int rev = 0,temp = n;
+ while (temp > 0) {
+        rev = rev * 10 + temp % 10;
+        temp = temp / 10;
+      }
+    return (n == rev);
+    }
+int main() 
+{
+   int num;
+ printf("Enter a number: ");
+    scanf("%d", &num);
+ if (palindrome(num))
+        printf("Palindrome Number");
+    else
+        printf("Not a Palindrome Number");      
+     return 0;
+       }
+     }
