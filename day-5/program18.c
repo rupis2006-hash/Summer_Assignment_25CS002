ab79b5113dea25eefa67d05817273166c4b42439
Rupali <rupis2006@gmail.com>
2026-06-08 18:25:18 +0530

    Create program18.c

---
+++ day-5/program18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+ int num, temp, rem, sum = 0, fact;
+ scanf("%d", &num);
+  temp = num;
+   while(temp != 0) {
+   rem = temp % 10;
+   fact = 1;
+    for(int i = 1; i <= rem; i++)
+     fact *= i;
+     sum += fact;
+     temp /= 10;
+      }
+     if(sum == num)
+     printf("Strong Number");
+     else
+     printf("Not a Strong Number");
+    return 0;
+     }
