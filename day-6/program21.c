0c1d7d497be48396e0f63927db6258a49442ee88
Rupali <rupis2006@gmail.com>
2026-06-08 18:36:20 +0530

    Create day-6/program21.c

---
+++ day-6/program21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int num, binary[32], i = 0;
+scanf("%d", &num);
+while(num > 0) {
+ binary[i] = num % 2;
+ num /= 2;
+  i++;
+   }
+ for(int j = i - 1; j >= 0; j--)
+  printf("%d", binary[j]);
+    return 0;
+     }
