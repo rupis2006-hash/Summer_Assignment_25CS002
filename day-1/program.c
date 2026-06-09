e0368435547f73c893703978f4d2b5f97891562e
Rupali <rupis2006@gmail.com>
2026-06-09 16:52:31 +0530

    Update program.c

--- day-1/program.c
+++ day-1/program.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
-int main() {
-    int N;
-    int sum;
-
-    printf("Enter a number: ");
-    scanf("%d", &N);
-
-    sum = N * (N + 1) / 2;
-
-    printf("Sum of first %d natural numbers = %d\n", N, sum);
-
+int main()
+ {
+   int n;
+   int sum;
+  printf("Enter a number: ");
+   scanf("%d", &N);
+  sum = N * (N + 1) / 2;
+printf("Sum of first %d natural numbers = %d\n", N, sum);
     return 0;
-}
+      }
