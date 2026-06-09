535c372524dba9b1bf53fa9bcf996cdd2fa9730a
Rupali <rupis2006@gmail.com>
2026-06-09 09:04:45 +0530

    Create program27.c

---
+++ day-7/program27.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int sum(int n)
+{
+ if (n == 0)
+  return 0;
+  return (n % 10) + sum(n / 10);
+  }
+  int main()
+   {
+   int n;
+   printf("Enter number: ");
+   scanf("%d", &n);
+   printf("Sum of digits = %d\n", sum(n));
+    return 0;
+     }
