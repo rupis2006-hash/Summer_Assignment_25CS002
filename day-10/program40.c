15856d23bd288975ef6defcaa402a67dbf08a135
Rupali <rupis2006@gmail.com>
2026-06-10 19:18:04 +0530

    Create program40.c

---
+++ day-10/program40.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+ int n=5,i,j;
+for(i=0;i<n;i++) 
+{
+for(j=0;j<n-i-1;j++)
+     printf(" ");
+for(j=0;j<=i;j++)
+   printf("%c",'A'+j);
+ for(j=i-1;j>=0;j--)
+   printf("%c",'A'+j);
+    printf("\n");
+       }
+   return 0;
+     }
