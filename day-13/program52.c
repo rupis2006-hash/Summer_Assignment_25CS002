80f036371d2d41d87e30fbfc4c627ac7bdd60946
Rupali <rupis2006@gmail.com>
2026-06-17 21:23:22 +0530

    Create program52.c

---
+++ day-13/program52.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+      int arr[5], i;
+       int even = 0, odd = 0;
+
+      printf("Enter 5 elements:\n");
+
+       for(i = 0; i < 5; i++)
+         {
+        scanf("%d", &arr[i]);
+        }
+     for(i = 0; i < 5; i++)
+        {
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even elements = %d\n", even);
+    printf("Odd elements = %d\n", odd);
+
+          return 0;
+          }
