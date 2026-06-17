191c04bbb596533525e365875e76ee4071148ca1
Rupali <rupis2006@gmail.com>
2026-06-17 21:20:24 +0530

    Create program50.c

---
+++ day-13/program50.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+  int main()
+    {
+    int arr[5], i, sum = 0;
+    float avg;
+
+    printf("Enter 5 elements:\n");
+    for(i = 0; i < 5; i++)
+        {
+        scanf("%d", &arr[i]);
+        sum = sum + arr[i];
+        }
+    avg = (float)sum / 5;
+    printf("Sum = %d\n", sum);
+    printf("Average = %.2f\n", avg);
+
+      return 0;
+       }
