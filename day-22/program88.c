f1cbf182bed02a94dd7651e079469e7d5e99b0b3
Rupali <rupis2006@gmail.com>
2026-07-04 11:16:37 +0530

    Create program88.c

---
+++ day-22/program88.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+    char str[100], result[100];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] != ' ') {
+            result[j] = str[i];
+            j++;
+        }
+    }
+
+    result[j] = '\0';
+
+    printf("String after removing spaces: %s", result);
+
+    return 0;
+}
